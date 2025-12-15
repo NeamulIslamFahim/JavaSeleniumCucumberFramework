8c29e8ad2223dd9aab272856948e52acf2595fac
neamulislamfahim <neamulislamfahim@gmail.com>
2025-12-15 23:16:51 +0600

    commit

--- src/test/java/BaseTest/BaseTest.java
+++ src/test/java/BaseTest/BaseTest.java
@@ -16,6 +16,9 @@ public class BaseTest {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
